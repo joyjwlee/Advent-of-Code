5d4963c6b9f763950e938669edcfad311ad269ff
Jaewon Lee <joyjwlee@gmail.com>
2020-12-17 09:44:03 -0800

    WIP // later

---
+++ 2020/Day 17 - Conway Cubes/main_part_1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// set up input
+ifstream file("input.txt");
+string line;
+
+// variables
+
+void readInput() {
+    while (getline(file, line)) {
+        //
+    }
+}
+
+int main(void) {
+    // read input
+    readInput();
+    return 0;
+}
