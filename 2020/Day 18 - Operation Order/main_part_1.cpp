afca1d7427773367799ed90d5b6cadc081606123
Jaewon Lee <joyjwlee@gmail.com>
2020-12-27 14:00:00 -0800

    WIP // later

--- 2020/Day 18 - Operation Order/main_part_1.cpp
+++ 2020/Day 18 - Operation Order/main_part_1.cpp
@@ -8,9 +8,9 @@ string line;
 
 // variables
 vector<string> expressions;
-int ans = 0;
-queue<int> nums;
-queue<char> ops;
+int ans = 0, n;
+stack<int> nums;
+stack<char> ops;
 
 void readInput() {
     while (getline(file, line)) {
@@ -19,7 +19,64 @@ void readInput() {
 }
 
 int calculate(string s) {
-    //
+    // loop through the expression
+    for (char c : s) {
+        // if open parenthesis
+        if (c == '(')
+            ops.push(c);
+        // if empty
+        else if (nums.empty())
+            nums.push((int)(c - '0'));
+        // if just number, do operation
+        else if ('0' <= c && c <= '9') {
+            // get last value
+            n = nums.top();
+            nums.pop();
+
+            // do one of four operations
+            if (ops.top() == '*')
+                n *= (int)(c - '0');
+            else if (ops.top() == '+')
+                n += (int)(c - '0');
+            else if (ops.top() == '/')
+                n /= (int)(c - '0');
+            else
+                n -= (int)(c - '0');
+
+            // put back and remove operator
+            nums.push(n);
+            ops.pop();
+        }
+        // if operator, add to ops
+        else if (c == '*' || c == '+' || c == '/' || c == '-')
+            ops.push(c);
+        // if closing parenthesis
+        else if (c == ')') {
+            // repeat above until reach open
+            while (ops.top() != '(') {
+                // get last value
+                n = nums.top();
+                nums.pop();
+
+                // do one of four operations
+                if (ops.top() == '*')
+                    n *= (int)(c - '0');
+                else if (ops.top() == '+')
+                    n += (int)(c - '0');
+                else if (ops.top() == '/')
+                    n /= (int)(c - '0');
+                else
+                    n -= (int)(c - '0');
+
+                // put back and remove operator
+                nums.push(n);
+                ops.pop();
+            }
+            // remove open paren
+            ops.pop();
+        }
+    }
+    return nums.top();
 }
 
 void solve() {
